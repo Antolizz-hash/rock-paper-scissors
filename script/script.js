bb54c67fc6825bf1772bc23b4d72d3e26d9ea20c
antony <antonyfwende1@gmail.com>
2026-03-30 01:59:47 -0400

    Remove console.log in the getComputerChoice function
    Remove console.log because it does not print anything inside the function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
-console.log('Hello! World!');
+function getComputerChoice(){
+    let computerChoice;
+    let choice = Math.floor(Math.random()*3)
+    if(choice ===0){
+        computerChoice ='rock';
+    }
+    else if(choice===1){
+        computerChoice = 'paper';
+    }
+    else{
+        computerChoice = 'scissors';
+    }
+    return computerChoice.toLowerCase();
+    console.log(computerChoice);
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanChoice = prompt('Enter either rock, paper, or scissors:').toLowerCase();
+    return humanChoice;
+     
+}
+// console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+
